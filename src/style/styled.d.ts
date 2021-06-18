39bd0f59aabacd761a96917da149f8cf11ef628b
Froyo-Swap <froyoswap@gmail.com>
2021-06-19 00:10:52 +0100

    style

--- src/style/styled.d.ts
+++ src/style/styled.d.ts
@@ -1,7 +1,7 @@
 // eslint-disable-next-line import/no-unresolved
-import { FroyoTheme } from '@pantherswap-libs/uikit/dist/theme'
+import { PantherTheme } from '@pantherswap-libs/uikit/dist/theme'
 
 declare module 'styled-components' {
   /* eslint-disable @typescript-eslint/no-empty-interface */
-  export interface DefaultTheme extends FroyoTheme {}
+  export interface DefaultTheme extends PantherTheme {}
 }
